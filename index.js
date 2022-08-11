ae3ecea10b258b4da2f95ec1b0c049e96dedc2ed
MahbubMorshed <mahbub.bogra30@gmail.com>
2022-08-10 19:58:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const fs = require("fs");
+const cors = require("cors");
+const express = require("express");
+const PythonShell = require("python-shell").PythonShell;
+
+const PORT = 80;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.post("/python", (req, res) => {
+  fs.writeFileSync("test.py", req.body.code);
+
+  const testCases = {
+    one: [1, 2, 3],
+    two: [2, 2, 4],
+    three: [2, -2, 0],
+  };
+
+  const promises = [];
+  const testCaseResults = [];
+
+  Object.keys(testCases).map((key) => {
+    promises.push(
+      new Promise((resolve, reject) => {
+        PythonShell.run(
+          "test.py",
+          {
+            mode: "text",
+            pythonOptions: ["-u"],
+            args: testCases[key],
+          },
+          function (err, results) {
+            if (err) {
+              reject();
+              throw err;
+            }
+            console.log(results);
+            testCaseResults.push(results[0]);
+            resolve(true);
+          }
+        );
+      })
+    );
+  });
+
+  Promise.all(promises).then(() => {
+    res.json({ testCaseResults });
+  });
+});
+
+// // import { PythonShell } from "python-shell";
+// const fs = require("fs");
+// const cors = require("cors");
+// const express = require("express");
+// const { PythonShell } = require("python-shell");
+// // const PythonShell = require("Python-Shell").PythonShell;
+// // const PythonShell = require("python-shell").PythonShell;
+
+// const port = 80;
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.post("/python", (req, res) => {
+//   fs.writeFileSync("test.py", req.body.code);
+
+//   let options = {
+//     mode: "text",
+//     pythonOptions: ["-u"], // get print results in real-time
+//     args: [1, 2, 3],
+//   };
+
+//   PythonShell.run("test.py", options, function (err, results) {
+//     if (err) throw err;
+//     // results is an array consisting of messages collected during execution
+
+//     res.json({ passOrFail: results[0] });
+//     console.log("results: %j", results);
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
